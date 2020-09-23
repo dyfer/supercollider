61d88b72c3712dceee0743c673b07c82cf51f7a9
Marcin Paczkowski <dyfeer@gmail.com>
2020-09-23 10:53:58 -0700

    Update qpm-test.sh

--- .travis/qpm-test.sh
+++ .travis/qpm-test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-echo "pip version"
-pip --version
-echo "pip2 version"  
-pip2 --version
-echo "pip3 version"
-pip3 --version
+# echo "pip version"
+# pip --version
+# echo "pip2 version"  
+# pip2 --version
+# echo "pip3 version"
+# pip3 --version
+QPM_DEBUG=1
+echo "\"hello\".postln; 0.exit" >> test.scd
+$SCLANG test.scd
 qpm test.run -l $TRAVIS_BUILD_DIR/BUILD/travis_test_run.json --path $SCLANG --include $HOME/Quarks
