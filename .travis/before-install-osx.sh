9ddee84fa7b1b3726f34f07458a02bc1ad5640a4
Marcin Paczkowski <dyfeer@gmail.com>
2020-09-23 16:52:15 -0700

    travis: don't upgrade qt5 in homebrew

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -6,7 +6,7 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 brew install libsndfile || brew install libsndfile || exit 1
 brew install portaudio || exit 2
 brew install ccache || exit 3
-brew upgrade qt5 || exit 4
+# we don't upgrade qt since we don't update homebrew
 brew link qt5 --force || exit 5
 brew install fftw --verbose # temp allow
 
