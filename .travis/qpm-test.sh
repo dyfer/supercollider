54d15806c2c69f4df888dc523c5f665025c0f5e8
Marcin Paczkowski <dyfeer@gmail.com>
2020-09-23 12:04:21 -0700

    Update qpm-test.sh

--- .travis/qpm-test.sh
+++ .travis/qpm-test.sh
@@ -5,7 +5,8 @@
 # pip2 --version
 # echo "pip3 version"
 # pip3 --version
-QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu" $SCLANG
+# QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu --enable-logging --log-level=0 --no-sandbox" $SCLANG
+QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu --enable-logging --log-level=0 --no-sandbox"
 QPM_DEBUG=1
 echo "\"hello\".postln; 0.exit" >> test.scd
 $SCLANG test.scd
