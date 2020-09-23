ac15fa1110cbf37bf6d265b6f3dd8ef2e1e75474
Marcin Paczkowski <dyfeer@gmail.com>
2020-09-23 12:21:33 -0700

    Update qpm-test.sh

--- .travis/qpm-test.sh
+++ .travis/qpm-test.sh
@@ -6,8 +6,8 @@
 # echo "pip3 version"
 # pip3 --version
 # QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu --enable-logging --log-level=0 --no-sandbox" $SCLANG
-QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu --enable-logging --log-level=0 --no-sandbox"
-QPM_DEBUG=1
+export QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu --enable-logging --log-level=0 --no-sandbox"
+export QPM_DEBUG=1
 echo "\"hello\".postln; 0.exit" >> test.scd
 $SCLANG test.scd
 qpm test.run -l $TRAVIS_BUILD_DIR/BUILD/travis_test_run.json --path $SCLANG --include $HOME/Quarks
