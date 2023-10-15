af9faca84a4eababafb8b52fdfa82e7c788e0d54
Marcin Pączkowski <dyfeer@gmail.com>
2023-10-15 14:57:33 -0700

    vcpkg readline static on x86

--- vcpkg/triplets/x86-windows-release.cmake
+++ vcpkg/triplets/x86-windows-release.cmake
@@ -2,3 +2,6 @@ set(VCPKG_TARGET_ARCHITECTURE x86)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_BUILD_TYPE release)
+if(PORT MATCHES "readline") # only specific libraries are build as shared
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
