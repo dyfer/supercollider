88a22ef02ecef80f973bb16ab8ddd488533f8eaa
Marcin Pączkowski <dyfeer@gmail.com>
2024-10-16 11:01:50 -0700

    vcpkg set flags manually

--- vcpkg/triplets/x64-arm64-osx-release-supercollider.cmake
+++ vcpkg/triplets/x64-arm64-osx-release-supercollider.cmake
@@ -5,7 +5,9 @@ if(PORT MATCHES "libsndfile|fftw3|readline") # only specific libraries are build
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
 
-
+set(VCPKG_CXX_FLAGS "-arch x86_64 -arch arm64")
+set(VCPKG_C_FLAGS "-arch x86_64 -arch arm64")
+set(VCPKG_LINKER_FLAGS "-arch x86_64 -arch arm64")
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES x86_64 arm64)
 set(VCPKG_BUILD_TYPE release)
