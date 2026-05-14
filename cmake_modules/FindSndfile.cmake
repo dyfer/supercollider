61f67a678b0424ccddd56727331cd17a6f89750e
Marcin Pączkowski <dyfeer@gmail.com>
2026-05-14 01:30:29 -0700

    cmake: update libsndfile implementation

--- cmake_modules/FindSndfile.cmake
+++ cmake_modules/FindSndfile.cmake
@@ -1,103 +1,91 @@
 # - Find sndfile
-# Find the native sndfile includes and libraries
-#
-#  SNDFILE_INCLUDE_DIR - where to find sndfile.h, etc.
-#  SNDFILE_LIBRARIES   - List of libraries when using libsndfile.
-#  SNDFILE_FOUND       - True if libsndfile found.
+# Search in vcpkg, pkgconfig, and system locations.
+# Create unified target Sndfile::sndfile
 
-if(NO_LIBSNDFILE)
-  #if(NOT SNDFILE_FIND_QUIETLY)
-  #  message(STATUS "FindSndfile: sndfile deactivated (NO_LIBSNDFILE)")
-  #endif()
-  set(SNDFILE_FOUND False)
-  set(SNDFILE_INCLUDE_DIR "nowhere")  # for onceonly check above
-  set(SNDFILE_LIBRARIES "")
-  add_definitions("-DNO_LIBSNDFILE")
-elseif (SNDFILE_INCLUDE_DIR AND SNDFILE_LIBRARY)
-  set(SNDFILE_LIBRARIES ${SNDFILE_LIBRARY})
-  set(SNDFILE_FOUND TRUE)
-
-elseif (APPLE)
-
-  find_path(SNDFILE_INCLUDE_DIR sndfile.h
-    HINTS /usr/local/opt/libsndfile/include
-  )
-
-  find_library(SNDFILE_LIBRARY NAMES libsndfile.dylib libsndfile.a
-    HINTS /usr/local/opt/libsndfile/lib
-  )
-
-  if(NOT SNDFILE_INCLUDE_DIR OR NOT SNDFILE_LIBRARY)
-    set(SNDFILE_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/../external_libraries/libsndfile/)
-    set(SNDFILE_LIBRARIES ${CMAKE_CURRENT_LIST_DIR}/../platform/mac/lib/scUBlibsndfile.a)
-    set(SNDFILE_FOUND TRUE)
-    add_definitions("-isystem ${CMAKE_CURRENT_LIST_DIR}/../external_libraries/libsndfile")
-    message(STATUS "Could not find install of libsndfile, using old bundled version instead")
-  else()
-    set(SNDFILE_FOUND TRUE)
-    message(STATUS "Found install of libsndfile in ${SNDFILE_LIBRARY}")
-    set(SNDFILE_LIBRARIES ${SNDFILE_LIBRARY})
-  endif()
-
-else()
-  find_path(SNDFILE_INCLUDE_DIR sndfile.h
-    HINTS
-      "${CMAKE_SOURCE_DIR}/../${CMAKE_LIBRARY_ARCHITECTURE}/libsndfile/include"
-      "$ENV{ProgramW6432}/Mega-Nerd/libsndfile/include"
-      "$ENV{ProgramFiles}/Mega-Nerd/libsndfile/include"
-      "$ENV{PROGRAMFILES\(X86\)}/libsndfile/include"
-      "$ENV{ProgramFiles}/libsndfile/include"
-    PATHS /usr/local/include
-      /usr/include
-  )
-
-  find_library(SNDFILE_LIBRARY
-    NAMES sndfile sndfile-1 libsndfile libsndfile-1
-    HINTS
-      "${CMAKE_SOURCE_DIR}/../${CMAKE_LIBRARY_ARCHITECTURE}/libsndfile/lib"
-      "${CMAKE_SOURCE_DIR}/../${CMAKE_LIBRARY_ARCHITECTURE}/libsndfile/bin"
-      "$ENV{ProgramW6432}/Mega-Nerd/libsndfile/lib"
-      "$ENV{ProgramW6432}/Mega-Nerd/libsndfile/bin"
-      "$ENV{ProgramFiles}/Mega-Nerd/libsndfile/lib"
-      "$ENV{ProgramFiles}/Mega-Nerd/libsndfile/bin"
-      "$ENV{PROGRAMFILES\(X86\)}/libsndfile/lib"
-      "$ENV{PROGRAMFILES\(X86\)}/libsndfile/bin"
-      "$ENV{ProgramFiles}/libsndfile/lib"
-      "$ENV{ProgramFiles}/libsndfile/bin"
-    PATHS /usr/local/
-      /usr/lib
-  )
-  # used by Windows only
-  find_path(SNDFILE_LIBRARY_DIR
-    NAMES libsndfile.dll libsndfile-1.dll sndfile.dll
-    HINTS
-      "${CMAKE_SOURCE_DIR}/../${CMAKE_LIBRARY_ARCHITECTURE}/libsndfile/lib"
-      "${CMAKE_SOURCE_DIR}/../${CMAKE_LIBRARY_ARCHITECTURE}/libsndfile/bin"
-      "$ENV{ProgramW6432}/Mega-Nerd/libsndfile/lib"
-      "$ENV{ProgramW6432}/Mega-Nerd/libsndfile/bin"
-      "$ENV{ProgramFiles}/Mega-Nerd/libsndfile/lib"
-      "$ENV{ProgramFiles}/Mega-Nerd/libsndfile/bin"
-      "$ENV{PROGRAMFILES\(X86\)}/libsndfile/lib"
-      "$ENV{PROGRAMFILES\(X86\)}/libsndfile/bin"
-      "$ENV{ProgramFiles}/libsndfile/lib"
-      "$ENV{ProgramFiles}/libsndfile/bin"
-    PATH_SUFFIXES "bin"
-  )
-
-  # Handle the QUIETLY and REQUIRED arguments and set SNDFILE_FOUND to TRUE if
-  # all listed variables are TRUE.
-    include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(Sndfile DEFAULT_MSG
-    SNDFILE_LIBRARY SNDFILE_INCLUDE_DIR)
-
-  if(SNDFILE_FOUND)
-    set(SNDFILE_LIBRARIES ${SNDFILE_LIBRARY})
-  else(SNDFILE_FOUND)
-    set(SNDFILE_LIBRARIES)
-  endif(SNDFILE_FOUND)
-
+if(DEFINED ENV{VCPKG_ROOT})
+    find_package(sndfile CONFIG QUIET)
 endif()
-mark_as_advanced(SNDFILE_INCLUDE_DIR SNDFILE_LIBRARY)
+
+# if sndfile was not found in vcpkg
+if(NOT TARGET Sndfile::sndfile)
+
+    # check pkgconfig first
+    find_package(PkgConfig QUIET)
+    if(PKG_CONFIG_FOUND)
+        pkg_check_modules(PC_SNDFILE QUIET sndfile)
+    endif()
+
+    find_path(SNDFILE_INCLUDE_DIR
+        NAMES sndfile.h
+        HINTS
+            ${PC_SNDFILE_INCLUDEDIR}
+            ${PC_SNDFILE_INCLUDE_DIRS}
+            "/usr/local/opt/libsndfile/include"
+            "$ENV{PROGRAMFILES\(X86\)}/libsndfile/include"
+            "$ENV{ProgramFiles}/libsndfile/include"
+        PATHS
+            /usr/local/include
+            /usr/include
+    )
+
+    find_library(SNDFILE_LIBRARY
+        NAMES sndfile sndfile-1 libsndfile libsndfile-1 libsndfile.dylib libsndfile.a
+        HINTS
+            ${PC_SNDFILE_LIBDIR}
+            ${PC_SNDFILE_LIBRARY_DIRS}
+            "/usr/local/opt/libsndfile/lib"
+            "$ENV{PROGRAMFILES\(X86\)}/libsndfile/lib"
+            "$ENV{PROGRAMFILES\(X86\)}/libsndfile/bin"
+            "$ENV{ProgramFiles}/libsndfile/lib"
+            "$ENV{ProgramFiles}/libsndfile/bin"
+        PATHS
+            /usr/local/lib
+            /usr/lib
+    )
+
+    if(WIN32)
+        find_program(SNDFILE_RUNTIME_DLL
+            NAMES libsndfile.dll libsndfile-1.dll sndfile.dll
+            HINTS
+                "$ENV{PROGRAMFILES\(X86\)}/libsndfile/bin"
+                "$ENV{ProgramFiles}/libsndfile/bin"
+        )
+    endif()
+
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(Sndfile
+        REQUIRED_VARS SNDFILE_LIBRARY SNDFILE_INCLUDE_DIR
+    )
+
+    if(NOT SNDFILE_FOUND)
+        message(FATAL_ERROR "Could not find sndfile.")
+    endif()
+
+    # create the unified target
+    add_library(Sndfile::sndfile UNKNOWN IMPORTED)
+    
+    # configure the target properties, including the DLL if found
+    if(WIN32 AND SNDFILE_RUNTIME_DLL)
+        set_target_properties(Sndfile::sndfile PROPERTIES
+            IMPORTED_LOCATION "${SNDFILE_RUNTIME_DLL}"
+            IMPORTED_IMPLIB   "${SNDFILE_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${SNDFILE_INCLUDE_DIR}"
+        )
+    else()
+        set_target_properties(Sndfile::sndfile PROPERTIES
+            IMPORTED_LOCATION "${SNDFILE_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${SNDFILE_INCLUDE_DIR}"
+        )
+    endif()
+
+    if(PC_SNDFILE_CFLAGS_OTHER)
+        set_target_properties(Sndfile::sndfile PROPERTIES
+            INTERFACE_COMPILE_OPTIONS "${PC_SNDFILE_CFLAGS_OTHER}"
+        )
+    endif()
+
+    mark_as_advanced(SNDFILE_INCLUDE_DIR SNDFILE_LIBRARY SNDFILE_RUNTIME_DLL)
+endif()
 
 # check for format support
 # note: this only check whehter libsndfile's _version_ supports the given format
@@ -106,7 +94,8 @@ mark_as_advanced(SNDFILE_INCLUDE_DIR SNDFILE_LIBRARY)
 include(CMakePushCheckState)
 cmake_push_check_state(RESET)
 
-list(APPEND CMAKE_REQUIRED_INCLUDES ${SNDFILE_INCLUDE_DIR})
+get_target_property(SNDFILE_TARGET_INCLUDES Sndfile::sndfile INTERFACE_INCLUDE_DIRECTORIES)
+set(CMAKE_REQUIRED_INCLUDES ${SNDFILE_TARGET_INCLUDES})
 
 include(CheckCSourceCompiles)
 check_c_source_compiles("
